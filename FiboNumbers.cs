52b15f86bebe39086c47b4c8eed938890f6e0fcf
KotsmanV <vassilis.kotsman@gmail.com>
2021-04-01 19:30:44 +0300

    Initial commit

---
+++ FiboNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciNumbers
+{
+    public class FiboNumbers
+    {
+        public static double FiboNext(double input)
+        {
+            double fOne=1;
+            double fTwo=fOne;
+            double fN=0;
+            for (int i = 1; i <= input; i++)
+            {
+                fN = fOne + fTwo;
+                fOne = fTwo;
+                fTwo = fN;
+            }
+            return fN;
+        }
+    }
+}
